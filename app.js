661ee76a22ec3b0d2945ebdb30334aff1e4fb174
Gabe Ragland <gabe.ragland@gmail.com>
2015-03-22 14:58:50 +0800

    Added style.js and show/hide results on input focus/blur

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var AppComponent = React.createClass({
     getInitialState: function(){
         return {
+            inFocus: false,
             endpoint: 'https://api.instagram.com/v1/users/search?access_token=291933.1fb234f.49bb69d84df4458dafeb45262b722d2a&q=gabe',
             results: [
                 {
@@ -18,14 +19,23 @@ var AppComponent = React.createClass({
 
         return (
             <div>
-                <InputComponent handleChange={this.handleChange}/>
-                <ResultsComponent data={this.state.results}/>
+                <InputComponent handleChange={this.handleChange} handleFocus={this.handleFocus} handleBlur={this.handleBlur}/>
+                <ResultsComponent data={this.state.results} visible={this.state.inFocus} />
             </div>
         );
     },
-    handleChange: function(data) {
-        this.setState( { results: data } );
+    componentDidMount: function(){
+        console.log('MOUNT');
     },
+    handleChange: function(data) {
+        this.setState( { results : data } );
+    },
+    handleFocus: function() {
+        this.setState( { inFocus : true } );
+    },
+    handleBlur: function() {
+        this.setState( { inFocus : false } );
+    }
 });
 
 
@@ -34,7 +44,7 @@ var ResultsComponent = React.createClass({
     render: function(){
 
         var resultNodes = this.props.data.map(function(result){
-            console.log(result);
+            //console.log(result);
             return (
                 <Result image={result.image}>
                     {result.username}
@@ -42,8 +52,13 @@ var ResultsComponent = React.createClass({
             );
         });
 
+        // Best way to hide/show results?
+        var resultsClass = (this.props.visible === true ? 'results show' : 'results hide');
+
+        console.log(resultsClass);
+
         return (
-            <ul>
+            <ul className={resultsClass}>
                 {resultNodes}
             </ul>
         );
@@ -69,9 +84,15 @@ var InputComponent = React.createClass({
     handleChange: function(event){
         this.props.handleChange(event.target.value);
     },
+    handleFocus: function(event){
+        this.props.handleFocus();
+    },
+    handleBlur: function(event){
+        this.props.handleBlur();
+    },
     render: function(){
         return (
-            <input type="text" value={this.props.name} onChange={this.handleChange} />
+            <input type="text" className="input-typeahead" value={this.props.name} onChange={this.handleChange} onFocus={this.handleFocus} onBlur={this.handleBlur}/>
         );
     }
 });
