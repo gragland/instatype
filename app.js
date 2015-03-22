ba7b0e9a5f27b1c8bbdf8b3dd388ee38087ae9e9
Gabe Ragland <gabe.ragland@gmail.com>
2015-03-22 14:07:10 +0800

    First commit!

---
+++ app.js
@@ -0,0 +1,82 @@
+var AppComponent = React.createClass({
+    getInitialState: function(){
+        return {
+            endpoint: 'https://api.instagram.com/v1/users/search?access_token=291933.1fb234f.49bb69d84df4458dafeb45262b722d2a&q=gabe',
+            results: [
+                {
+                    image : 'http://imgfave-herokuapp-com.global.ssl.fastly.net/image_cache/142642156144625.jpg',
+                    username : 'Gabe'
+                },
+                {
+                    image : 'http://40.media.tumblr.com/37a9787b9352ba10c2dfc8916813a71f/tumblr_nkth875MO41qjqyo8o1_500.jpg',
+                    username : 'Irvin'
+                }
+            ],
+        }
+    },
+    render: function(){
+
+        return (
+            <div>
+                <InputComponent handleChange={this.handleChange}/>
+                <ResultsComponent data={this.state.results}/>
+            </div>
+        );
+    },
+    handleChange: function(data) {
+        this.setState( { results: data } );
+    },
+});
+
+
+var ResultsComponent = React.createClass({
+
+    render: function(){
+
+        var resultNodes = this.props.data.map(function(result){
+            console.log(result);
+            return (
+                <Result image={result.image}>
+                    {result.username}
+                </Result>
+            );
+        });
+
+        return (
+            <ul>
+                {resultNodes}
+            </ul>
+        );
+    }
+});
+
+
+var Result = React.createClass({
+    
+    render: function(){
+
+        return (
+            <li>
+                <img width="30" src={this.props.image}/>
+                {this.props.children}
+            </li>
+        );
+    }
+});
+
+
+var InputComponent = React.createClass({
+    handleChange: function(event){
+        this.props.handleChange(event.target.value);
+    },
+    render: function(){
+        return (
+            <input type="text" value={this.props.name} onChange={this.handleChange} />
+        );
+    }
+});
+
+React.render(
+   <AppComponent/>,
+  document.getElementById('mount-point')
+);
