ce593687f7fd12ffdb8fb0ed8564ca30f8459660
Gabe Ragland <gabe.ragland@gmail.com>
2015-04-27 20:17:11 +0700

    Restructured project so that example site lives in examples directory and the InstaType component (src/app.js) now has module.exports so that it can be included with require().

---
+++ examples/instagram/js/app.js
@@ -0,0 +1,20 @@
+var React = require('react');
+var CustomFunctions = require('./custom-functions.js');
+var InstaTypeComponent = require('../../../src/js/app.js');
+
+React.initializeTouchEvents(true);
+
+CustomFunctions.resultSelected( { id : 478987666 } );
+
+React.render(
+  <InstaTypeComponent 
+    placeholder="Search instagram users" 
+    endpoint="https://api.instagram.com/v1/users/search" 
+    dataKeys={window.dataKeys}
+    customFunctions={CustomFunctions}
+    loadingIcon="/./images/loading.gif"
+    limit={6} 
+    thumbStyle="circle"/>,
+
+  document.getElementById('app')
+);
