530965eb274ed5291e57ecbd00795a9af5f83ea0
Gabe Ragland <gabe.ragland@gmail.com>
2015-04-27 20:24:58 +0700

    Fixed loading icon path for gh-pages

--- examples/instagram/js/bundle.js
+++ examples/instagram/js/bundle.js
@@ -14,7 +14,7 @@ React.render(React.createElement(InstaTypeComponent, {
   endpoint: 'https://api.instagram.com/v1/users/search',
   dataKeys: window.dataKeys,
   customFunctions: CustomFunctions,
-  loadingIcon: '/./images/loading.gif',
+  loadingIcon: '../../images/loading.gif',
   limit: 6,
   thumbStyle: 'circle' }), document.getElementById('app'));
 
@@ -19998,6 +19998,7 @@ var InstaTypeComponent = React.createClass({
       resultsId: null // Unique identifier for set of results (used by ResultsComponent.shouldComponentUpdate)
     };
   },
+
   getDefaultProps: function getDefaultProps() {
     return {
       text: false,
